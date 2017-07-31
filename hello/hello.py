bee2c034db89ce3db66a34e293012a2bd5f6faea
yale8848 <royal8848@163.com>
2017-07-31 19:35:56 +0800

    add test

--- hello/hello.py
+++ hello/hello.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import Student
 
 def power(x, n):
     s = 1
@@ -14,4 +19,40 @@ def add_end(L=None):
 def add(x, y, f):
     return f(x) + f(y)
 
-print(add(-5, 6, abs))
+
+s = Student.Student()
+s.score = 60
+
+#print(json.dumps(s.__dict__))
+
+
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] Consuming %s...' % n)
+        r = '200 OK'
+
+def produce(c):
+    c.send(None)
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return: %s' % r)
+    c.close()
+
+#c = consumer()
+#produce(c)
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+bicycles.append('aaa')
+bicycles.insert(2,'cc')
+del bicycles[2]
+bicycles.sort(reverse=True)
+for b in bicycles:
+    print(b)
+print(bicycles)
